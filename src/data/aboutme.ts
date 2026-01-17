05fe62746bee38a7f39e7291fbc2e388bd054d6b
Byeongju Woo <byeongju@umich.edu>
2026-01-18 00:30:02 +0900

    remove some shortcut

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -59,9 +59,9 @@ export const aboutMe: AboutMe = {
     // ",
     // from 26.01.17
     "I am an incoming Ph.D. student in Computer Science and Engineering at the University of Michigan, \
-    advised by Prof. <a href='https://web.eecs.umich.edu/~stellayu/'>Stella X. Yu</a>, starting Fall 2026.<br /><br /> \
-    I am currently serving as a First Lieutenant in the Republic of Korea Air Force (ROKAF) \
-    and working as a research officer at the Agency for Defense Development (ADD), the Korean counterpart to the U.S. DARPA. \
+    advised by Prof. <a href='https://web.eecs.umich.edu/~stellayu/'>Stella Yu</a>, starting Fall 2026.<br /><br /> \
+    I am currently serving as a First Lieutenant in the Republic of Korea Air Force \
+    and working as a research officer at the Agency for Defense Development, the Korean counterpart to the U.S. DARPA. \
     Previously, I completed my bachelor's degree (<em>Summa Cum Laude</em>) \
     in Computer Science and Engineering at POSTECH. \
     ",
